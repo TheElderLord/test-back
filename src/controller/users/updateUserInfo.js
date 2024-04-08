06258f41b88e4548833dcaaf379d6ac194d122dd
TheElderLord <61541224+TheElderLord@users.noreply.github.com>
2024-04-08 18:30:33 +0500

    fixes

--- src/controller/users/updateUserInfo.js
+++ src/controller/users/updateUserInfo.js
@@ -1,9 +1,9 @@
 const query = require("../../db/connection");
 
 const updateUser = async (authLogin, body) => {
-    const { username, password, email } = body;
+    const { username, password, email,firstname,lastname } = body;
     const user = await query(
-      `UPDATE users SET login = '${username}', password = '${password}', email = '${email}' WHERE login = '${authLogin}'`
+      `UPDATE users SET login = '${username}', password = '${password}',firstname = '${firstname}',lastname = '${lastname}', email = '${email}' WHERE login = '${authLogin}'`
     );
     return user;
   };
